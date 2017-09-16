dc31cade0a31e51fea5b5072dc97da8a1f51568d
zhujian <zhujian@xiaomai5.com>
2017-09-16 15:31:33 +0800

    Fixed

--- web/src/core/config.js
+++ web/src/core/config.js
@@ -2,7 +2,7 @@
  * @Author: zhujian 
  * @Date: 2017-08-13 01:10:00 
  * @Last Modified by: zhujian
- * @Last Modified time: 2017-09-15 09:58:28
+ * @Last Modified time: 2017-09-16 15:24:51
  */
 
 function CONF() {
@@ -10,7 +10,7 @@ function CONF() {
   //    server: 'http://10.0.0.15:8082/',
   // 站点目录
   this.rootPath = window.location.host == "/";
-  this.api = SERVER || 'http://116.196.124.92:8080/';
+  this.api = SERVER || 'http://127.0.0.1:8080/';
   // 开发构建输出目录
   this.scriptPath = 'dist/js/';
   // 脚本、样式更新时间

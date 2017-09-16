5bee7d6e53021acb43dc402835141baab55aec4e
zhujian <zhujian@xiaomai5.com>
2017-09-16 18:20:23 +0800

    Fixed

--- node-server/controller/order/get.js
+++ node-server/controller/order/get.js
@@ -2,7 +2,7 @@
  * @Author: zhujian 
  * @Date: 2017-08-13 01:11:30 
  * @Last Modified by: zhujian
- * @Last Modified time: 2017-09-16 18:14:44
+ * @Last Modified time: 2017-09-16 18:19:37
  */
 import Order from '../../models/order/index'
 class GetOrder {
@@ -18,21 +18,21 @@ class GetOrder {
         query = { status: '6' }
       } else if (req.body.type == 3) {
         query = {
-          status: { '$ne': '6', 'Material': { '$elemMatch': { 'orderTime': { '$gt': moment().startOf('day').valueOf(), '$lt': moment().endOf('day').valueOf() } } } }
+          status: { '$ne': '6' }, 'Material': { '$elemMatch': { 'orderTime': { '$gt': moment().startOf('day').valueOf(), '$lt': moment().endOf('day').valueOf() } } }
         }
       }
-        console.log(query)
-        const orderList = await Order.find(query, null, { sort: { modifyTime: -1 } })
+      console.log(query)
+      const orderList = await Order.find(query, null, { sort: { modifyTime: -1 } })
 
-        res.send({
-          status: 1,
-          data: orderList
-        })
-        return;
-      } catch (err) {
-        console.log('取不到学员数据了', err);
-      }
+      res.send({
+        status: 1,
+        data: orderList
+      })
+      return;
+    } catch (err) {
+      console.log('取不到学员数据了', err);
     }
+  }
 }
 
 export default new GetOrder()
